922954e874afcd72355dcec689be9a32ec29bc07
justinthadathil <justinthadathil93@gmail.com>
2023-07-23 18:51:36 +0100

    final done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -14,7 +13,6 @@ import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
